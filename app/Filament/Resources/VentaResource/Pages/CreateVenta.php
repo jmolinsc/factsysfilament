9758e9cb4e6a5d749ba4a122a2fa02c667b0dafe
jmolinsc <centenojulio@hotmail.com>
2024-07-29 16:31:29 -0600

    ACTION CUSTOM CREATEVENTA

--- app/Filament/Resources/VentaResource/Pages/CreateVenta.php
+++ app/Filament/Resources/VentaResource/Pages/CreateVenta.php
@@ -9,4 +9,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateVenta extends CreateRecord
 {
     protected static string $resource = VentaResource::class;
+
+    protected function getFormActions(): array
+    {
+        return array_merge(parent::getFormActions(), [
+            Actions\Action::make('Afectar')
+                ->action(function () {
+
+                })
+            ],
+        );
+    }
 }
