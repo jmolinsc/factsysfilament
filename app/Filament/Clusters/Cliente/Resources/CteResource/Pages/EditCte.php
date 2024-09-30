e9d1a685f01e0b37b307ebfec3690945372a1bc4
jmolinsc <centenojulio@hotmail.com>
2024-09-29 18:56:49 -0600

    PLUGGIN NAVIGATION

--- app/Filament/Clusters/Cliente/Resources/CteResource/Pages/EditCte.php
+++ app/Filament/Clusters/Cliente/Resources/CteResource/Pages/EditCte.php
@@ -5,16 +5,22 @@ namespace App\Filament\Clusters\Cliente\Resources\CteResource\Pages;
 use App\Filament\Clusters\Cliente\Resources\CteResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use JoseEspinal\RecordNavigation\Traits\HasRecordNavigation;
 
 class EditCte extends EditRecord
 {
+    use HasRecordNavigation;
     protected static string $resource = CteResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [
+
+
+       /* $existingActions = [
             Actions\DeleteAction::make(),
-        ];
+        ];*/
+
+        return array_merge(parent::getActions(), $this->getNavigationActions());
     }
 
     protected function getRedirectUrl(): string
--- app/Filament/Clusters/Cliente/Resources/CteResource/Pages/ListCtes.php
+++ app/Filament/Clusters/Cliente/Resources/CteResource/Pages/ListCtes.php
@@ -5,9 +5,12 @@ namespace App\Filament\Clusters\Cliente\Resources\CteResource\Pages;
 use App\Filament\Clusters\Cliente\Resources\CteResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use JoseEspinal\RecordNavigation\Traits\HasRecordsList;
 
 class ListCtes extends ListRecords
 {
+
+    use HasRecordsList;
     protected static string $resource = CteResource::class;
 
     protected function getHeaderActions(): array
